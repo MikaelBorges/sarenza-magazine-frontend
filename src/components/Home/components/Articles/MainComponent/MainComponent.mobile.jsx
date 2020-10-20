61227a0f872246151cd175dac3dffe8e76ce7f8f
LOIC GUIDOU <lguidou@sarenza.com>
2020-10-20 21:33:36 +0200

    fix(homestyle): add css mobile for home

---
+++ src/components/Home/components/Articles/MainComponent/MainComponent.mobile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ArticleTitle from '../ArticleTitle/ArticleTitle';
+import styles from './MainComponent.module.scss';
+
+const MainComponent = ({ article }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <ArticleTitle
+          title={article.title}
+          author={article.author}
+          publishDate={article.publishDate}
+          firstArticle
+        />
+      </div>
+      <img src={article.image} className={styles.image} alt={article.title} />
+    </div>
+  );
+};
+
+export default MainComponent;
