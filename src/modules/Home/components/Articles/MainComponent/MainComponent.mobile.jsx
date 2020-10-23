8e484274127c59cc448d9a65df26cb2826d52207
Hachim <hac.remini@gmail.com>
2020-10-23 09:21:19 +0200

    fix(switch): add switch mobile desktop

--- src/modules/Home/components/Articles/MainComponent/MainComponent.mobile.jsx
+++ src/modules/Home/components/Articles/MainComponent/MainComponent.mobile.jsx
@@ -1,21 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
+
+import Text from '@/components/commons/Text';
 
 import ArticleTitle from '../ArticleTitle/ArticleTitle.mobile';
 import styles from './MainComponent.mobile.module.scss';
 
 const MainComponent = ({ article }) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <ArticleTitle
-          title={article.title}
-          author={article.author}
-          publishDate={article.publishDate}
-          firstArticle
-        />
+  return article.link ? (
+    <Link href={encodeURI(article.link)}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <ArticleTitle
+            title={article.title}
+            author={article.author}
+            publishDate={article.publishDate}
+            firstArticle
+          />
+        </div>
+        <img src={article.image} className={styles.image} alt={article.title} />
       </div>
-      <img src={article.image} className={styles.image} alt={article.title} />
-    </div>
+    </Link>
+  ) : (
+    <Text>Aucun Article</Text>
   );
 };
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
-import Home, { initProps } from "../modules/Home/Home"
+import { HOME_QUERY_ALL } from '../apollo/queries/home/homeQuery';
+import Home from '../modules/Home/Home';
+import HomeMobile from '../modules/Home/Home.mobile';
 
-export async function getServerSideProps() {
-  
-  let currentProps = await initProps()
-  return {
-    props: { 
-      ...currentProps.props
-    }
-  }
+export default function HomePage({ isMobile }) {
+  return (
+    <div className="layout">
+      {isMobile ? <HomeMobile query={HOME_QUERY_ALL} /> : <Home query={HOME_QUERY_ALL} />}
+    </div>
+  );
 }
 
 export default Home
