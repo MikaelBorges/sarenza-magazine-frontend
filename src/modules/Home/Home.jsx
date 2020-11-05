4872af40446c014c59d201d32ca5ae68db739bcc
Hachim <hac.remini@gmail.com>
2020-11-05 22:06:43 +0100

    fix(marquee): fix marquee

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -12,56 +12,64 @@ import Header from './components/Header/Header';
 const Home = ({ data }) => {
   return (
     <>
-      { data ? <Pagination
-        data={data.articles}
-        renderContent={(articles) => (
-          <>
-            <Header header={data.header} />
-            <MainComponent article={data.firstArticle} />
-            {data.marqueeTop && <Marquee
-              marquee={{
-                fastAnimation: false,
-                slowAnimation: false,
-                text: data.marqueeTop[0].text
-              }}
-            />}
+      {data ? (
+        <Pagination
+          data={data.articles}
+          renderContent={(articles) => (
+            <>
+              <Header header={data.header} />
+              <MainComponent article={data.firstArticle} />
+              {data.marqueeTop && (
+                <Marquee
+                  marquee={{
+                    fastAnimation: false,
+                    slowAnimation: false,
+                    text: data.marqueeTop.text
+                  }}
+                />
+              )}
 
-            <Articles articles={articles.slice(0, 5)} position={1} />
-            {data.displayFirst && <Display
-              text={data.displayFirst.text}
-              button={{
-                label: data.displayFirst.button.label,
-                url: data.displayFirst.button.link
-              }}
-              whiteTheme={data.displayFirst.whiteTheme}
-            />
-            }
+              <Articles articles={articles.slice(0, 5)} position={1} />
+              {data.displayFirst && (
+                <Display
+                  text={data.displayFirst.text}
+                  button={{
+                    label: data.displayFirst.button.label,
+                    url: data.displayFirst.button.link
+                  }}
+                  whiteTheme={data.displayFirst.whiteTheme}
+                />
+              )}
 
-            <Articles articles={articles.slice(5, 10)} position={2} />
-            {data.displaySecond && <Display
-              text={data.displaySecond.text}
-              button={{
-                label: data.displaySecond.button.label,
-                url: data.displaySecond.button.link
-              }}
-              whiteTheme={data.displaySecond.whiteTheme}
-            />
-            }
-            {data.marquee ? (
-              <Marquee
-                marquee={{
-                  fastAnimation: false,
-                  slowAnimation: false,
-                  text: data.marquee[0].text
-                }}
-                margin
-              />
-            ) : null}
-            <Articles articles={articles.slice(10, 12)} position={1} />
-          </>
-        )}
-        renderFooter={(dataCast, params) => <PaginationComponent params={params} />}
-      />: <></>}
+              <Articles articles={articles.slice(5, 10)} position={2} />
+              {data.displaySecond && (
+                <Display
+                  text={data.displaySecond.text}
+                  button={{
+                    label: data.displaySecond.button.label,
+                    url: data.displaySecond.button.link
+                  }}
+                  whiteTheme={data.displaySecond.whiteTheme}
+                />
+              )}
+              {data.marquee ? (
+                <Marquee
+                  marquee={{
+                    fastAnimation: false,
+                    slowAnimation: false,
+                    text: data.marquee.text
+                  }}
+                  margin
+                />
+              ) : null}
+              <Articles articles={articles.slice(10, 12)} position={1} />
+            </>
+          )}
+          renderFooter={(dataCast, params) => <PaginationComponent params={params} />}
+        />
+      ) : (
+        <></>
+      )}
     </>
   );
 };
