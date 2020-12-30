d19fcfa6ce53396778b8c989f1f168cefa5be565
Mikael BORGES <mborges-externe@sarenza.com>
2020-12-30 22:15:44 +0100

    fix(productlinemobile): fix behavior and style

--- src/modules/Article/components/ProductLine/ProductLine.mobile.jsx
+++ src/modules/Article/components/ProductLine/ProductLine.mobile.jsx
@@ -1,41 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Image from '@/components/commons/Image/Image';
-import Slider from 'react-slick';
-
 import styles from './ProductLine.mobile.module.scss';
-const ProductLine = ({ ProductCard }) => {
-  console.log(ProductCard);
-  const settings = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    paddingCenter: true,
 
-    responsive: [
-      {
-        breakpoint: 975,
-        settings: 'unslick'
-      }
-    ]
-  };
+const ProductLineMobile = ({ ProductCard }) => {
+
+  const [favoriteStatus, setFavoriteStatus] = useState(false);
+  function handleClick(e) {
+    e.preventDefault();
+    setFavoriteStatus(!favoriteStatus);
+  }
+
   return (
-    <div className={styles.SliderContainer}>
-      <Slider {...settings} className={styles.cards}>
+    <div className={styles.vignetteContainer}>
         {ProductCard.map((card) => {
-          console.log('ProductLine -> ProductCard', ProductCard);
           return (
-            <div className={styles.card}>
-              <Image src={card.url_image} alt="Photo" />
-              <a className={styles.btn} href={card.url_cta}>
-                Voir le produit
-              </a>
+            <div className={styles.vignette}>
+              <div className={styles.imageContainer}>
+                {card.url_image && (
+                  <Image src={card.url_image} alt="image-product-line" />
+                )}
+                {card.url_cta && (
+                  <a
+                    className={`button darkseid pictenza pictenza-basket ${styles.gellule}`}
+                    href={card.url_cta}
+                    role="button"
+                    tabIndex={-1}
+                  >
+                    <span className={styles.onlyText}>{card.url_text}</span>
+                  </a>
+                )}
+                {card.pcid && (
+                  <span
+                    className={`pictenza pictenza-favorites ${styles.favoriteCard} ${
+                      favoriteStatus ? styles.pictenzaFavoritesSelect : ''
+                    }`}
+                    data-pcid={card.pcid}
+                    onClick={(e) => handleClick(e)}
+                    role="button"
+                    tabIndex={-2}
+                  >
+                  </span>
+                )}
+              </div>
             </div>
           );
         })}
-      </Slider>
+
+          {/* <div className={styles.vignette}>
+            <div className={styles.imageContainer}>
+              <Image src="https://cdn.sarenza.net/_img/productsv4/0000264771/0000264771_484173_09_504x690.jpg" alt="image-product-line" />
+              <a className={`button darkseid pictenza pictenza-basket ${styles.gellule}`}>
+                <span className={styles.onlyText}>Voir tous les produits</span>
+              </a>
+              <span
+                className={`pictenza pictenza-favorites ${styles.favoriteCard} ${
+                  favoriteStatus ? styles.pictenzaFavoritesSelect : ''
+                }`}
+                data-pcid="6"
+                onClick={(e) => handleClick(e)}
+                role="button"
+                tabIndex={-2}
+              >
+              </span>
+            </div>
+          </div> */}
+
     </div>
   );
 };
 
-export default ProductLine;
+ProductLineMobile.propTypes = {
+  ProductCard: PropTypes.arrayOf(
+    PropTypes.shape({
+      url_image: PropTypes.string,
+      url_cta: PropTypes.string,
+      url_text: PropTypes.string,
+      pcid: PropTypes.string
+    })
+  )
+};
+
+ProductLineMobile.defaultProps = {
+  ProductCard: []
+};
+
+export default ProductLineMobile;
