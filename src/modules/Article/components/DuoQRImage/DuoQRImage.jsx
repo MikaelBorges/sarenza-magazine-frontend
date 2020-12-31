3a995c568cd38f2eda50dbffc104d35d61c3ef92
Mikael BORGES <mborges-externe@sarenza.com>
2020-12-31 12:22:47 +0100

    feat(duoqrimage): proptypes + control data

--- src/modules/Article/components/DuoQRImage/DuoQRImage.jsx
+++ src/modules/Article/components/DuoQRImage/DuoQRImage.jsx
@@ -1,32 +1,51 @@
 import Markdown from 'markdown-to-jsx';
 import React from 'react';
 import styles from './DuoQRImage.module.scss';
+import PropTypes from 'prop-types';
 
 const DuoQRImage = ({ Title, Text, Image }) => {
   return (
     <div className={styles.duoQR}>
-        <div className={styles.imageContainer}>
+        {Image && (
+          <div className={styles.imageContainer}>
             <img className={styles.image} src={Image.url} alt={Image.alt} />
-        </div>
+          </div>
+        )}
         <div className={styles.textContainer}>
+          {Title && (
             <h2 className={`title-edito2 ${styles.mainTitle}`}>{Title}</h2>
+          )}
+          {Text && (
             <Markdown
-            options={{
-              overrides: {
-                p: { props: { className: styles.paragraph } },
-                h1: { props: { className: 'title-edito2' } },
-                h2: { props: { className: 'title-edito2' } },
-                h3: { props: { className: 'title-edito2' } },
-                h4: { props: { className: 'title-edito2' } },
-                h5: { props: { className: 'title-edito2' } },
-                h6: { props: { className: 'title-edito2' } }
-              }
-            }}>
+              options={{
+                overrides: {
+                  p: { props: { className: styles.paragraph } },
+                  h1: { props: { className: 'title-edito2' } },
+                  h2: { props: { className: 'title-edito2' } },
+                  h3: { props: { className: 'title-edito2' } },
+                  h4: { props: { className: 'title-edito2' } },
+                  h5: { props: { className: 'title-edito2' } },
+                  h6: { props: { className: 'title-edito2' } }
+                }
+              }}>
             {Text}
             </Markdown>
+          )}
         </div>
     </div>
   );
+};
+
+DuoQRImage.propTypes = {
+  Title: PropTypes.string,
+  Text: PropTypes.string,
+  Image: PropTypes.string
+};
+
+DuoQRImage.defaultProps = {
+  Title: '',
+  Text: '',
+  Image: ''
 };
 
 export default DuoQRImage;
--- src/modules/Article/components/DuoQRImage/DuoQRImage.mobile.jsx
+++ src/modules/Article/components/DuoQRImage/DuoQRImage.mobile.jsx
@@ -1,29 +1,48 @@
 import Markdown from 'markdown-to-jsx';
 import React from 'react';
 import styles from './DuoQRImage.mobile.module.scss';
+import PropTypes from 'prop-types';
 
 const DuoQRImageMobile = ({ Title, Text, Image }) => {
   return (
     <div className={styles.duoQR}>
+      {Title && (
         <h2 className={`title-edito2 ${styles.mainTitle}`}>{Title}</h2>
+      )}
+      {Image && (
         <img className={styles.image} src={Image.url} alt={Image.alt} />
+      )}
+      {Text && (
         <Markdown
-            className={styles.richTextContainer}
-            options={{
-              overrides: {
-                p: { props: { className: styles.paragraph } },
-                h1: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
-                h2: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
-                h3: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
-                h4: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
-                h5: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
-                h6: { props: { className: `title-edito2 ${styles.secondaryTitles}` } }
-              }
-            }}>
-            {Text}
+          className={styles.richTextContainer}
+          options={{
+            overrides: {
+              p: { props: { className: styles.paragraph } },
+              h1: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
+              h2: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
+              h3: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
+              h4: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
+              h5: { props: { className: `title-edito2 ${styles.secondaryTitles}` } },
+              h6: { props: { className: `title-edito2 ${styles.secondaryTitles}` } }
+            }
+          }}>
+        {Text}
         </Markdown>
+      )}
     </div>
   );
+};
+
+DuoQRImageMobile.propTypes = {
+  Title: PropTypes.string,
+  Text: PropTypes.string,
+  Image: PropTypes.string
+};
+
+DuoQRImageMobile.defaultProps = {
+  Title: '',
+  Text: '',
+  Image: ''
 };
 
 export default DuoQRImageMobile;
