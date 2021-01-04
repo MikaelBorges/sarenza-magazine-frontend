8cb90e0abb3be81cb5fa94766b3a199c10f6c3bb
LOIC GUIDOU <lguidou@sarenza.com>
2021-01-04 16:46:00 +0100

    fix(blocEdito): button is now optionnal

--- src/modules/Article/components/BlocEditoCourt/BlocEditoCourt.jsx
+++ src/modules/Article/components/BlocEditoCourt/BlocEditoCourt.jsx
@@ -4,17 +4,15 @@ import styles from './BlocEditoCourt.module.scss';
 import Mardown from 'markdown-to-jsx';
 
 const BlocEditoCourt = ({ Title, Text, Image, Button }) => {
-  return Title && Text && Image && Button ? (
+  return Title && Text && Image ? (
     <div className={styles.wrapper}>
       <div className={styles.boxButton}>
         <div className={styles.blocText}>
-                  <h2 className={styles.title}>{Title}</h2>
-                  <Mardown options={{ forceInline: false }}>{Text}</Mardown>
-          {Button && (
-            <a href={Button.link} className={styles.link}>
-              {Button.label}
-            </a>
-          )}
+          <h2 className={styles.title}>{Title}</h2>
+          <Mardown options={{ forceInline: false }}>{Text}</Mardown>
+         { Button && <a href={Button.link} className={styles.link}>
+            {Button.label}
+          </a>}
         </div>
       </div>
       {Image && <img src={Image.url} className={styles.visual} alt={Image.alt} />}
--- src/modules/Article/components/BlocEditoCourt/BlocEditoCourt.mobile.jsx
+++ src/modules/Article/components/BlocEditoCourt/BlocEditoCourt.mobile.jsx
@@ -4,18 +4,18 @@ import styles from './BlocEditoCourt.mobile.module.scss';
 import Mardown from 'markdown-to-jsx';
 
 const BlocEditoCourt = ({ Title, Text, Image, Button }) => {
-  return Title && Text && Image && Button ? (
+  return Title && Text && Image ? (
     <div className={styles.wrapper}>
       <div className={styles.boxButton}>
         <div className={styles.blocText}>
           <h2 className={styles.title}>{Title}</h2>
           <Mardown options={{ forceInline: false }}>{Text}</Mardown>
         </div>
-        {Button && (
+        {Button && 
           <a href={Button.link} className={styles.link}>
             {Button.label}
           </a>
-        )}
+        }
       </div>
       {Image && <img src={Image.url} className={styles.visual} alt={Image.alt} />}
     </div>
