269ed679ea45bb93019cf8c9d4f8a91a63cdbb0a
Yolo <ylorho@sarenza.com>
2021-01-22 17:05:40 +0100

    Add version

--- deploy-re7.ps1
+++ deploy-re7.ps1
@@ -1,7 +1,14 @@
-# docker-compose build --force-rm --no-cache
-# az acr login -n srzlab
-# docker-compose push
+$version=Read-Host "Please enter a target version"
+
+Write-Host $version
+$Env:VERSION = $version
+
+docker-compose build
+
+az acr login -n srzlab
+docker-compose push
+
 gcloud container clusters get-credentials srz-gke-hp-99aad13f --zone europe-west1-b --project srz-prj-a5a67395
-helm upgrade -i --wait magazine ./helm/sarenza-digitalexperience-magazine --tiller-namespace digitalexperience-re7-tiller --namespace digitalexperience-re7
-# helm upgrade --recreate-pods -i magazine-re7 ./helm/sarenza-digitalexperience-magazine --tiller-namespace digitalexperience-dev-tiller --namespace digitalexperience-dev
-# --recreate-pods
+
+# --recreate-pods to force update pods
+helm upgrade -i --set image.tag=$version --wait magazine ./helm/sarenza-digitalexperience-magazine --tiller-namespace digitalexperience-re7-tiller --namespace digitalexperience-re7
