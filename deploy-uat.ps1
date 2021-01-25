1c7bea2ad424722c7191de611a608c3eba1a8828
Yolo <ylorho@sarenza.com>
2021-01-25 19:43:51 +0100

    sds

--- deploy-uat.ps1
+++ deploy-uat.ps1
@@ -1,5 +1,16 @@
-docker-compose build --force-rm --no-cache
+$version=Read-Host "Please enter a target version"
+
+$replicaCount=2
+
+Write-Host $version
+$Env:VERSION = $version
+
+docker-compose build
+
 az acr login -n srzlab
 docker-compose push
+
 gcloud container clusters get-credentials srz-gke-hp-99aad13f --zone europe-west1-b --project srz-prj-a5a67395
-helm upgrade -i --wait magazine ./helm/sarenza-digitalexperience-magazine --tiller-namespace digitalexperience-uat-tiller --namespace digitalexperience-uat
+
+# --recreate-pods to force update pods
+helm upgrade -i --set replicaCount=$replicaCount --set image.tag=$version --wait magazine ./helm/sarenza-digitalexperience-magazine --tiller-namespace digitalexperience-uat-tiller --namespace digitalexperience-uat
