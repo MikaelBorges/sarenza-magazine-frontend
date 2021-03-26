fab771b58ad0695d0aaeb0b4350186be10f3c355
Fabien Hauchecorne <fhauchecorne@sarenza.com>
2021-03-26 17:34:49 +0100

    Add conditionnal video openning vs Socials cookie consent

--- src/modules/Article/components/IframeVideo/IframeVideo.jsx
+++ src/modules/Article/components/IframeVideo/IframeVideo.jsx
@@ -1,10 +1,14 @@
 import Image from '@/components/commons/Image/Image';
 import React, { useState, useCallback } from 'react';
 import styles from './IframeVideo.module.scss';
+import getCookieConsentList from '../../../../utils/getCookieConsentList';
 
-const IframeVideo = ({ image, iframe_url }) => {
+const IframeVideo = ({ image, iframe_url, iframe_page }) => {
   const [play, setPlay] = useState(false);
-  const handlePlay = useCallback(() => setPlay(true), []);
+  const handlePlay = useCallback(() => {
+    const cookieConsentList = getCookieConsentList();
+    cookieConsentList.includes('Socials') ? setPlay(true) : window.open(iframe_page || iframe_url);
+  });
   return (
     <div className={styles.video}>
       {play && iframe_url ? (
